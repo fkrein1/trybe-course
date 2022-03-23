32e30755e8b9075fff5913b55c0166a03377925b
fkrein1 <felipekrein@gmail.com>
2022-03-22 22:13:14 -0300

    finaliza dia 02 bloco 10

--- 01-fundamentos/10-API-async/exercicio-02.js
+++ 01-fundamentos/10-API-async/exercicio-02.js
@@ -1,14 +1,14 @@
 //2. Agora que tal um exercício menos guiado? Vamos consultar uma API que fornece os valores de crypto moedas e mostrar as 10 primeiras.
 
 
-// const API_URL = 'https://api.coincap.io/v2/assets'
+const API_URL = 'https://api.coincap.io/v2/assets'
 
 // const fetchCoins = () => {
 //   fetch(API_URL)
 //     .then((response) => response.json())
 //     .then((data) => {
 //       data.data.forEach(({ name, symbol, priceUsd, marketCapUsd}, index) => {
-//         if (index < 10) {
+//         if (index < 15) {
 //           let div = document.createElement('div');
 //           let section = document.querySelector('#coins');
 //           div.innerHTML = `${index+1}: ${name} (${symbol})<br>Price: USD ${Number(priceUsd).toFixed(2)}<br> Market Cap: USD ${(Number(marketCapUsd)/ 1000000000).toFixed(2)} bi`;
@@ -20,8 +20,6 @@
 // }
 
 // window.onload =  () => fetchCoins()
-
-const API_URL = 'https://api.coincap.io/v2/assets'
 
 const fetchCoins = () => {
   const coinData = fetch(API_URL)
