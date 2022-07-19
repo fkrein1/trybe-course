9ca63c35d369bd98de023aae5d608d5d1c339a19
fkrein1 <felipekrein@gmail.com>
2022-07-19 19:55:36 -0300

    finaliza bloco 21 dia 01

--- 03-back-end/21-sql-joins/01-functions/exercicio.sql
+++ 03-back-end/21-sql-joins/01-functions/exercicio.sql
@@ -35,10 +35,28 @@ SELECT department_id, AVG(salary), COUNT(employee_id) FROM hr.employees
 GROUP BY department_id
 HAVING COUNT(employee_id) > 10;
 
-SET SQL_SAFE_UPDATES = 0;
-
 UPDATE hr.employees
 SET phone_number = REPLACE(phone_number, '515', '777')
 WHERE phone_number LIKE '515%';
 
-SET SQL_SAFE_UPDATES = 1;
+SELECT first_name FROM hr.employees
+WHERE CHAR_LENGTH(first_name) >= 8;
+
+SELECT employee_id, first_name, YEAR(hire_date)
+FROM hr.employees;
+
+SELECT employee_id, first_name, DAY(hire_date)
+FROM hr.employees;
+
+SELECT employee_id, first_name, MONTH(hire_date)
+FROM hr.employees;
+
+SELECT CONCAT(UCASE(first_name), ' ', UCASE(last_name)) AS full_name
+FROM hr.employees;
+
+SELECT last_name, hire_date
+FROM hr.employees
+WHERE YEAR(hire_date) = 1987 AND MONTH(hire_date) = 7;
+
+SELECT first_name, last_name, DATEDIFF(CURRENT_DATE(), hire_date) AS 'days_worked'
+FROM hr.employees;
