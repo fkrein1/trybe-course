8c6ceb143fd33613abb9492d901edd4341239db8
fkrein1 <felipekrein@gmail.com>
2022-07-30 09:58:03 -0300

    finaliza bloco 22 dia 04

--- 03-back-end/22-node-intro/04-express/exercicios/simpsons.js
+++ 03-back-end/22-node-intro/04-express/exercicios/simpsons.js
@@ -9,5 +9,31 @@ async function getSimpsons() {
   return JSON.parse(data);
 }
 
+app.get('/simpsons', async (req, res) => {
+  const data = await getSimpsons();
+  res.status(200).json(data);
+});
+
+app.get('/simpsons/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await getSimpsons();
+  const char = data.find((char) => char.id === id);
+  if (!char)
+    return res.status(400).json({ message: 'Personagem não encontrado' });
+  res.status(200).json(char);
+});
+
+app.post('/simpsons', async (req, res) => {
+  const { id, name } = req.body;
+  const data = await getSimpsons();
+  const char = data.find((char) => char.id === id);
+  if (char)
+    return res
+      .status(409)
+      .json({ message: 'Conflito, personagem já cadastrado' });
+  res.status(204).end();
+  data.push({ id, name });
+  fs.writeFile('./simpsons.json', JSON.stringify(data, null, 2));
+});
 
 app.listen(3001);
