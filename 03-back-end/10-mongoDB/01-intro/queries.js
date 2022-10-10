0664415a67e7d2b66a5044dfa43826f74bb81093
fkrein1 <felipekrein@gmail.com>
2022-10-10 16:13:46 -0300

    mongo day02

--- 03-back-end/10-mongoDB/01-intro/queries.js
+++ 03-back-end/10-mongoDB/01-intro/queries.js
@@ -1,32 +1,43 @@
+// docker pull mongo
+// docker run --name mongo-teste -d -p 27017:27017 mongo
+
+// docker exec -it mongo-teste monosh
+
+//show dbs
+// use nomeDoBanco 
+
 // Ex01
-db.bios.find({_id: 8})
+db.bios.find({ _id: 8 });
 
 // Ex02
-db.bios.find({_id: 8}, {name: 1})
+db.bios.find({ _id: 8 }, { name: 1 });
 
 //Ex03
-db.bios.find({_id: 8}, {_id: 0, name: 1, birth: 1})
+db.bios.find({ _id: 8 }, { _id: 0, name: 1, birth: 1 });
 
 //Ex04
-db.bios.find({"name.first": "John"}).pretty()
+db.bios.find({ 'name.first': 'John' }).pretty();
 
 //Ex05
-db.bios.find().limit(3).pretty()
+db.bios.find().limit(3).pretty();
 
 //Ex06
-db.bios.find().skip(5).limit(2)
+db.bios.find().skip(5).limit(2);
 
 //Ex 07
 // docker cp books.json mongo-teste:/tmp/books.json
-// docker exec -it mongo-teste bash    
+// docker exec -it mongo-teste bash
 // mongoimport -d bios -c books --file /tmp/books.json
-db.books.countDocuments({})
+db.books.countDocuments({});
 
 // Ex08
-db.books.countDocuments({status: "PUBLISH"})
+db.books.countDocuments({ status: 'PUBLISH' });
 
 // Ex09
-db.books.find({}, {title: 1, isbn: 1, _id: 0, pageCount: 1}).limit(3)
+db.books.find({}, { title: 1, isbn: 1, _id: 0, pageCount: 1 }).limit(3);
 
 // Ex10
-db.books.find({status: "MEAP"}, {title: 1, authors: 1, status: 1}).skip(5).limit(10)
+db.books
+  .find({ status: 'MEAP' }, { title: 1, authors: 1, status: 1 })
+  .skip(5)
+  .limit(10);
